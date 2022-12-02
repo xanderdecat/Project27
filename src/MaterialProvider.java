b25ac8132a0241917e7f1ed53f9d908280428593
xanderdecat <xander@decat.be>
2022-12-02 16:08:15 +0100

    User update

--- src/MaterialProvider.java
+++ src/MaterialProvider.java
@@ -13,7 +13,7 @@ public class MaterialProvider extends Provider {
     private ArrayList<Material> materials;
 
     // constructor
-    public MaterialProvider(String firstName, String lastName, LocalDate dateOfBirth, ArrayList<String> emails, ArrayList<String> phoneNumber, String companyName, String VATnumber, String accountNumber, String streetName, int houseNumber, int ZIP, String city, String country, LocalDate activityDate, URL linkToPage, ArrayList<Material> materials) {
+    public MaterialProvider(String firstName, String lastName, LocalDate dateOfBirth, ArrayList<String> emails, ArrayList<String> phoneNumber, String companyName, String VATnumber, String accountNumber, String streetName, int houseNumber, int ZIP, String city, String country, LocalDate activityDate, URL linkToPage, ArrayList<Material> materials) {   // in de constructor materials list?
         super(firstName, lastName, dateOfBirth, emails, phoneNumber, companyName, VATnumber, accountNumber, streetName, houseNumber, ZIP, city, country);
         this.matPNumber = helpMatPNumber++;
         this.activityDate = activityDate;

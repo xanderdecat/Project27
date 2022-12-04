cd87cc4468cd8a19a9fed5b31f8ee72a7c26202e
xanderdecat <xander@decat.be>
2022-12-04 17:54:37 +0100

    update @xander

--- src/GUI.java
+++ src/GUI.java
@@ -1,17 +1,6 @@
-import javax.swing.*;
+
 
 public class GUI {
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame();
 
-        frame.setTitle("EventApp");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(true);
-        frame.setSize(420, 420);
-        frame.setVisible(true);
-
-        
-
-    }
 }
